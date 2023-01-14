c4793b7dd197c54a1ceddf8a9403eafc3e2fd2d9
AhnSangEon <stdleaf@gmail.com>
2023-01-14 17:57:59 +0900

    c++ 커스텀메서드

--- UTILS.cpp
+++ UTILS.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 문자열 delimiter로 나누기.
 vector<string> split(string str, char delimiter) {
   string tmp;
   stringstream st(str);
@@ -11,4 +12,17 @@ vector<string> split(string str, char delimiter) {
   }
 
   return result;
+}
+
+// true: swap X, false: swap O.
+bool compare(string s1, string s2) {
+  return s1 < s2;
+}
+
+int main() {
+  vector<string> v = {"A910", "145C"};
+  sort(v.begin(), v.end(), compare);
+  for (string s: v) {
+    cout << s;
+  }
 }
